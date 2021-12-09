b4a1efec2f296f7bda35591f382b7fa7c73a59b0
razganna <95350518+razganna@users.noreply.github.com>
2021-12-09 14:48:46 +0300

    Update mongodb.sh

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -1,10 +1,9 @@
-set -euo pipefail
-IFS=$'\n\t'
+#!/bin/bash
+set -e
 
+# Install MongoDB
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
-apt update
-apt install -y mongodb-org
-
-systemctl start mongod
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt-get install -y mongodb-org
 systemctl enable mongod
